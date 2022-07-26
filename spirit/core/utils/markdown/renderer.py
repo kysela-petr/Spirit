1c3c0b4680508eae9bd8b8fa80f3d4685a9cd1f3
Kysela Petr <petr@kysela.biz>
2022-07-26 15:52:40 +0200

    Add fancybox to images

--- spirit/core/utils/markdown/renderer.py
+++ spirit/core/utils/markdown/renderer.py
@@ -72,9 +72,9 @@ class Renderer(mistune.Renderer):
 
         if title:
             title = escape(title)
-            html = '<img src="%s" alt="%s" title="%s"' % (src, text, title)
+            html = '<img src="%s" alt="%s" title="%s" data-fancybox="comment"] data-src="%s"' % (src, text, title, src)
         else:
-            html = '<img src="%s" alt="%s"' % (src, text)
+            html = '<img src="%s" alt="%s" data-fancybox="comment"] data-src="%s"' % (src, text, src)
 
         if self.options.get('use_xhtml'):
             return '%s />' % html
